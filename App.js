4843e79390795e14461890e53e3502933a75e47c
Will Munoz <willdmunoz@gmail.com>
2018-04-27 21:25:28 -0400

    Commit #3

--- App.js
+++ App.js
@@ -8,7 +8,8 @@ function getDayOfWeek(date) {
   return isNaN(dayOfWeek) ? null : ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'][dayOfWeek+1];
 }
 
-export default class clockApp extends Component {
+
+class Clock extends Component{
   constructor(props){
     super(props);
     this.state = {
@@ -19,43 +20,84 @@ export default class clockApp extends Component {
 
   componentDidMount() {
     setTimeout(() => {
-			this.setState({
-				time: moment().format("LTS"),
-			});
-		}, 1000);
+      this.setState({
+        time: moment().format("LTS")
+      });
+    }, 1000);
   }
+
+  render(){
+    return(
+        <Text style={styles.timeOutput}>
+			    {this.state.time}
+		    </Text>
+    )
+  }
+}
+
+class CalenderPrompt extends Component{
+  constructor(props){
+    super(props);
+    this.state = {
+      date: "1999-12-31"
+    };
+  }
+  
+
+  render(){
+    return(
+      <DatePicker 
+            style={styles.dateInput}
+            date = {this.state.date}
+            mode = "date"
+            placeholder = "Select Date"
+            format = "YYYY-MM-DD"
+            minDate = "1900-01-01"
+            maxDate = "2100-12-31"
+            confirmBtnText="Confirm"
+            cancelBtnText="Cancel"
+            customStyles={{
+              dateIcon: {
+                position: 'absolute',
+                left: 0,
+                top: 4,
+                marginLeft: 0
+              },
+              dateInput: {
+                marginLeft: 36
+              }
+            }}
+            onDateChange = {(date) => {this.setState({date: date})}}
+          />
+    )
+  }
+}
+
+class DateOutput extends Component{
+  constructor(props){
+    super(props);
+    this.state = {
+      dates: ""
+    }
+  }
+
+  render(){
+    return(
+      <Text style={styles.dateOutput}>
+          {getDayOfWeek(4)+" "+6}
+      </Text>
+    )
+  }
+}
+
+export default class clockFace extends Component {
 
   render() {
     return (
       <View style = {styles.container}>
-        <DatePicker 
-          style={styles.dateInput}
-          mode = "date"
-          placeholder = "Select Date"
-          format = "YYYY-MM-DD"
-          minDate = "1900-01-01"
-          maxDate = "2100-12-31"
-          confirmBtnText="Confirm"
-          cancelBtnText="Cancel"
-          customStyles={{
-            dateIcon: {
-              position: 'absolute',
-              left: 0,
-              top: 4,
-              marginLeft: 0
-            },
-            dateInput: {
-              marginLeft: 36
-            }
-          }}
-          onDateChange = {(date) => {this.setState({date: date})}}
-        />
-        <Text style={styles.timeOutput}>
-					{this.state.time}
-				</Text>
-        <Text style={styles.dateOutput}>
-          {getDayOfWeek(this.state.date)+" "+this.state.date}
-        </Text>
+        <CalenderPrompt/>
+        <Clock/>
+        <DateOutput/>
       </View>
     );
   }
@@ -86,3 +128,5 @@ export default class clockApp extends Component {
      alignItems: 'center',
    }
  })
+
+ 
